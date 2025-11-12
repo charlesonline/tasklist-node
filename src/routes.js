edf73199cec6fbfafe2f2d87981079afdb82a9f5
Charles S Ferreira <ferreiracsf@gmail.com>
2025-11-12 00:14:56 -0300

    novas validações feitas com yup

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth.js';
 
 const routes = new Router();
 
@@ -8,8 +9,11 @@ routes.get('/', (req, res) => {
     return res.json({ message: 'Welcome to the Task List API' });
 });
 
-routes.post('/users', UserController.store);
-
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
 
 export default routes;
